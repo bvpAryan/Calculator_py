69e70f604e05e5d3cd4ac1a4cab073024ae230b4
Aryanbvp <108211622+bvpAryan@users.noreply.github.com>
2025-08-05 15:41:18 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,59 @@
+#Calculator
+from colorama import Fore, Back, Style, init
+
+init(autoreset=True)
+
+while True:
+    try:
+        a = int(input(" Enter 1st number: "))
+        break
+    except:
+        print()   #-----
+        print(Fore.RED+ "  Invalid input, NUMBERS only")
+        
+
+print() #------
+while True:
+    try:
+        b = int(input(" Enter 2nd number: "))
+        break
+    except:
+        print()  #------
+        print(Fore.RED+ "  Invalid input, NUMBERS only")
+        
+
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+
+print()  #-----
+
+while True:
+    g = input(" Operation( + , - , * , / )?")
+
+    if g == "+":
+        print()  #-----
+        print(Fore.BLACK+ Back.YELLOW+ "---------FINAL_RESULT--------")
+        print("             SUM:", c)
+        break
+    elif g == "-":
+        print()  #-----
+        print("---------FINAL_RESULT--------")
+        print("  MINUS:", d)
+        break
+    elif g == "*":
+        print()  #-----
+        print("---------FINAL_RESULT--------")
+        print("  MULTIPLY:", e)
+        break
+    elif g == "/":
+        print()  #-----
+        print("---------FINAL_RESULT--------")
+        print("  DIVIDE:", f)
+        break
+    else:
+        print()  #-------
+        print(Fore.RED+ " invalid Operation")
+        
+        
